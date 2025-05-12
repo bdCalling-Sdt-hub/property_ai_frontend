ee45ee71038e39502ee55ebe61d58bee68591f04
apurbo <apurboroy7077@gmail.com>
2025-05-12 14:35:48 +0600

    updated environment variables

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,10 @@ import { Toaster } from "sonner";
 import "./index.css";
 import { store } from "./redux/store";
 import router from "./routes/routes";
+import { VITE_GOOGLE_MAP_API_KEY } from "./config/environment_variables";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <LoadScript googleMapsApiKey="AIzaSyBwapnpvig2OudZciAgqtdize9IowNV4Lw">
+    <LoadScript googleMapsApiKey={VITE_GOOGLE_MAP_API_KEY}>
       <Provider store={store}>
         <RouterProvider router={router} />
         <Toaster richColors position="top-center" />
