65a9ecfbd8e3a36a7a9e0732c6b19293ba3143f7
apurbo <apurboroy7077@gmail.com>
2025-05-12 16:28:52 +0600

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
+import dotenv from "dotenv";
+dotenv.config();
+const VITE_HOST = process.env.VITE_HOST;
+const VITE_PORT = process.env.VITE_PORT;
+const APP_NAME = process.env.APP_NAME;
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+console.log(`${APP_NAME} has started on http://${VITE_HOST}:${VITE_PORT}`);
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "10.0.60.100",
-    port:8080,
+    host: VITE_HOST,
+    port: VITE_PORT,
   },
 });
