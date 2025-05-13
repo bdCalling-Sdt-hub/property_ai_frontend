df39744589487f7d908b1f3134d0ee741d92ad06
apurbo <apurboroy7077@gmail.com>
2025-05-13 16:19:28 +0600

    updated vite.config.js for running the build to the .env mentioned port

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,17 @@ const VITE_PORT = process.env.VITE_PORT;
 const APP_NAME = process.env.APP_NAME;
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+
 console.log(`${APP_NAME} has started on http://${VITE_HOST}:${VITE_PORT}`);
+
 export default defineConfig({
   plugins: [react()],
   server: {
     host: VITE_HOST,
     port: VITE_PORT,
+  },
+  preview: {
+    host: VITE_HOST,
+    port: VITE_PORT, // Add this line for preview
   },
 });
